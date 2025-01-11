fa5c9074eef78ae6c967f2a6a5d2cdeb48b9a250
epicfire77 <parekhjay6824@students.esuhsd.org>
2025-01-11 13:23:50 -0800

    sidebar for yt vid

---
+++ src/watch.js
@@ -0,0 +1,23 @@
+
+let x = document.createElement('div');
+
+// add style (text white) to x
+x.style.color = 'white';
+
+
+let t = document.createTextNode("Paragraph is created.");
+x.appendChild(t);
+
+x.innerHTML = "<div class = \"sidebar\"> Hello, world! </div>";
+
+let button = document.createElement('button');
+button.innerHTML = "Click Me";
+button.onclick = function() {
+    alert("Button was clicked!");
+};
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+sleep(2000).then(() => { document.querySelector("#player-container-inner").querySelector("#player-container").appendChild(x); });
