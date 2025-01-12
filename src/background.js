7204ea9526fd5b4e745655cfa8a0ba70bdc7ab25
epicfire77 <parekhjay6824@students.esuhsd.org>
2025-01-12 02:13:33 -0800

    Co-authored-by: Prajwal Vandana <PrajwalVandana@users.noreply.github.com> Co-authored-by: Arjun <iarjun.sahlot@gmail.com>

--- src/background.js
+++ src/background.js
@@ -1,102 +1,118 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import {
-	getFirestore,
-	doc,
-	getDoc,
-	setDoc,
-	updateDoc,
-	deleteDoc,
-	collection,
-	addDoc,
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  deleteDoc,
+  collection,
+  addDoc,
+  arrayUnion,
 } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 
 export const firebaseConfig = {
-	apiKey: "AIzaSyCOi5de8KDLAbOGQZsLHI06QIIYfFTyoLo",
-	authDomain: "wetube-1edf0.firebaseapp.com",
-	projectId: "wetube-1edf0",
-	storageBucket: "wetube-1edf0.appspot.com",
-	messagingSenderId: "549613659675",
-	appId: "1:549613659675:web:4e03048fbcba0a7a2d3abe",
+  apiKey: "AIzaSyCOi5de8KDLAbOGQZsLHI06QIIYfFTyoLo",
+  authDomain: "wetube-1edf0.firebaseapp.com",
+  projectId: "wetube-1edf0",
+  storageBucket: "wetube-1edf0.appspot.com",
+  messagingSenderId: "549613659675",
+  appId: "1:549613659675:web:4e03048fbcba0a7a2d3abe",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 async function fetchDocument(collectionName, docId) {
-	try {
-		const docRef = doc(db, collectionName, docId);
-		const snap = await getDoc(docRef);
-		return snap.exists() ? snap.data() : null;
-	} catch (error) {
-		console.error(`Error fetching ${collectionName}/${docId}:`, error);
-		throw new Error(`Failed to fetch ${collectionName}`);
-	}
+  try {
+    const docRef = doc(db, collectionName, docId);
+    const snap = await getDoc(docRef);
+    return snap.exists() ? snap.data() : null;
+  } catch (error) {
+    console.error(`Error fetching ${collectionName}/${docId}:`, error);
+    throw new Error(`Failed to fetch ${collectionName}`);
+  }
 }
 
 async function setDocument(collectionName, docId, data) {
-	try {
-		const docRef = doc(db, collectionName, docId);
-		await setDoc(docRef, data, { merge: true });
-		return { success: true };
-	} catch (error) {
-		console.error(`Error setting ${collectionName}/${docId}:`, error);
-		throw new Error(`Failed to set ${collectionName}`);
-	}
+  try {
+    const docRef = doc(db, collectionName, docId);
+    await setDoc(docRef, data, { merge: true });
+    return { success: true };
+  } catch (error) {
+    console.error(`Error setting ${collectionName}/${docId}:`, error);
+    throw new Error(`Failed to set ${collectionName}`);
+  }
 }
 
 async function addDocument(collectionName, data) {
-	try {
-		const collRef = collection(db, collectionName);
-		const docRef = await addDoc(collRef, data);
-		return { success: true, id: docRef.id };
-	} catch (error) {
-		console.error(`Error add to ${collectionName}:`, error);
-		throw new Error(`Failed to add to ${collectionName}`);
-	}
+  try {
+    const collRef = collection(db, collectionName);
+    const docRef = await addDoc(collRef, data);
+    return { success: true, id: docRef.id };
+  } catch (error) {
+    console.error(`Error add to ${collectionName}:`, error);
+    throw new Error(`Failed to add to ${collectionName}`);
+  }
 }
 
 async function deleteDocument(collectionName, docId) {
-	try {
-		const docRef = doc(db, collectionName, docId);
-		await deleteDoc(docRef);
-		return { success: true };
-	} catch (error) {
-		console.error(`Error deleting ${collectionName}/${docId}:`, error);
-		throw new Error(`Failed to delete ${collectionName}`);
-	}
+  try {
+    const docRef = doc(db, collectionName, docId);
+    await deleteDoc(docRef);
+    return { success: true };
+  } catch (error) {
+    console.error(`Error deleting ${collectionName}/${docId}:`, error);
+    throw new Error(`Failed to delete ${collectionName}`);
+  }
 }
 
 const databaseFunctions = {
-	getUser: (userId) => fetchDocument("users", userId),
-	getVideo: (videoId) => fetchDocument("videos", videoId),
-	getParty: (partyId) => fetchDocument("parties", partyId),
+  getUser: (userId) => fetchDocument("users", userId),
+  getVideo: (videoId) => fetchDocument("videos", videoId),
+  getParty: (partyId) => fetchDocument("parties", partyId),
 
-	setUser: (userId, data) => setDocument("users", userId, data),
-	setVideo: (videoId, data) => setDocument("videos", videoId, data),
-	setParty: (partyId, data) => setDocument("parties", partyId, data),
+  setUser: (userId, data) => setDocument("users", userId, data),
+  setVideo: (videoId, data) => setDocument("videos", videoId, data),
+  setParty: (partyId, data) => setDocument("parties", partyId, data),
 
-	addUser: (data) => addDocument("users", data),
-	addVideo: (data) => addDocument("videos", data),
-	addParty: (data) => addDocument("parties", data),
+  addUser: (data) => addDocument("users", data),
+  addVideo: (data) => addDocument("videos", data),
+  addParty: (data) => addDocument("parties", data),
 
-	deleteUser: (userId) => deleteDocument("users", userId),
-	deleteVideo: (videoId) => deleteDocument("videos", videoId),
-	deleteParty: (partyId) => deleteDocument("parties", partyId),
+  // addMessage: async (partyId, data) => {
+	// 	console.log("in addMessage");
+  //   try {
+  //     const partyRef = doc(db, "parties", partyId);
+	// 		console.log(partyRef)
+  //     await updateDoc(partyRef, {
+  //       messages: arrayUnion(data),
+  //     });
+	// 		console.log("successfully added message to party");
+  //     return { success: true };
+  //   } catch (error) {
+  //     console.error(`Error adding message ${data} to ${partyId}:`, error);
+  //     throw new Error(`Failed to add message to ${partyId}`);
+  //   }
+  // },
+
+  deleteUser: (userId) => deleteDocument("users", userId),
+  deleteVideo: (videoId) => deleteDocument("videos", videoId),
+  deleteParty: (partyId) => deleteDocument("parties", partyId),
 };
 
 async function handleRequest(action, payload) {
-	const fetchFunction = databaseFunctions[action];
-	if (!fetchFunction) throw new Error("Invalid action");
-	let response = await fetchFunction(...payload);
-	return response;
+  const fetchFunction = databaseFunctions[action];
+  if (!fetchFunction) throw new Error("Invalid action");
+  return await fetchFunction(...payload);
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	const { action, payload } = request;
+  const { action, payload } = request;
 
-	handleRequest(action, payload)
-		.then((data) => sendResponse({ data }))
-		.catch((error) => sendResponse({ error: error.message }));
+  handleRequest(action, payload)
+    .then((data) => sendResponse({ data }))
+    .catch((error) => sendResponse({ error: error.message }));
 
-	return true;
+  return true;
 });
--- src/watch.js
+++ src/watch.js
@@ -10,19 +10,19 @@ let chatTitleWrapper = document.createElement("div");
 chatTitleWrapper.textContent = "{user}'s Watch Party Chat";
 chatTitleWrapper.className = "sidebar";
 chatTitleWrapper.style =
-	"box-shadow: none; border-radius: 10px 10px 0 0; padding: 15px; border: none; border-bottom: rgb(63, 63, 63) 1px solid; margin: 0;";
+  "box-shadow: none; border-radius: 10px 10px 0 0; padding: 15px; border: none; border-bottom: rgb(63, 63, 63) 1px solid; margin: 0;";
 chatFrame.appendChild(chatTitleWrapper);
 
 partyChatContainer.appendChild(chatFrame);
 
 let chatMessagesWrapper = document.createElement("div");
-chatMessagesWrapper.style = "height: 600px;";
+chatMessagesWrapper.className = "messages-wrapper";
 chatFrame.appendChild(chatMessagesWrapper);
 
 let chatInput = document.createElement("form");
 chatInput.className = "sidebar";
 chatInput.style =
-	"box-shadow: none; border-radius: 0 0 10px 10px; padding: 10px 24px; border: none; border-top: rgb(63, 63, 63) 1px solid; margin: 0; align-elements: left; text-align: left;";
+  "box-shadow: none; border-radius: 0 0 10px 10px; padding: 10px 24px; border: none; border-top: rgb(63, 63, 63) 1px solid; margin: 0; align-elements: left; text-align: left;";
 
 let chatTextBox = document.createElement("input");
 chatTextBox.type = "text";
@@ -33,49 +33,91 @@ let chatSubmitButton = document.createElement("button");
 chatSubmitButton.type = "submit";
 chatSubmitButton.className = "sendbutton";
 chatSubmitButton.innerHTML =
-	'<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%; fill: white;"><path d="M5 12 3 3l19 9-19 9 2-9zm.82.93-1.4 6.29L19.66 12 4.42 4.78l1.4 6.29L17 12l-11.18.93z" fill-rule="evenodd"></path></svg>';
+  '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%; fill: white;"><path d="M5 12 3 3l19 9-19 9 2-9zm.82.93-1.4 6.29L19.66 12 4.42 4.78l1.4 6.29L17 12l-11.18.93z" fill-rule="evenodd"></path></svg>';
 
 chatSubmitButton.style = "margin-left: 10px; vertical-align: middle;";
 
 chatInput.appendChild(chatSubmitButton);
+// Set a max height for the messages wrapper
 
+let observer = new MutationObserver(() => {
+    chatMessagesWrapper.scrollTop = chatMessagesWrapper.scrollHeight;
+});
+
+observer.observe(chatMessagesWrapper, { childList: true });
 function sendMessage(party, user, content) {
-	// send message to firestore
-	// add a string "{user} {content}" to the messages array field of the party
-	console.log(`${party}: ${user}: ${content}`);
+  // send message to firestore
+  // add a string "{user} {content}" to the messages array field of the party
+  console.log(`${party}: ${user}: ${content}`);
 
-	chrome.runtime.sendMessage(
-		{
-			action: "addData",
-			payload: {
-				doc: `parties/${party}`,
-				field: "messages",
-				type: "append",
-				content: user + " " + content,
-			},
-		},
-		(response) => {
-			response.success ? console.log("Data added successfully!") : console.log("Data not added successfully!");
-		}
-	);
+  chrome.runtime
+    .sendMessage({
+      action: "getParty",
+      payload: [party],
+    })
+    .then((response) => {
+      console.log(response);
+      let messages = response.data.messages || [];
+      messages.push(`${user} ${content}`);
+
+      chrome.runtime
+        .sendMessage({
+          action: "setParty",
+          payload: [party, { messages: messages }],
+        })
+        .then((response) => {
+          console.log(response);
+        });
+    });
 }
-
 chatInput.onsubmit = function (e) {
-	e.preventDefault();
-	// alert(chatTextBox.value);
-	sendMessage("testParty1", "testUser1", chatTextBox.value);
-	chatTextBox.value = "";
+    e.preventDefault();
+    sendMessage("testParty1", "testUser1", chatTextBox.value);
+    chatTextBox.value = "";
+    setTimeout(() => {
+        chatMessagesWrapper.scrollTop = chatMessagesWrapper.scrollHeight;
+    }, 500); // Adding a slight delay to ensure the message is added before scrolling
 };
 
 chatFrame.appendChild(chatInput);
 
 initPartyChat();
 
+// get messages from firestore every 1 second
+setInterval(() => {
+  chrome.runtime
+    .sendMessage({
+      action: "getParty",
+      payload: ["testParty1"],
+    })
+    .then((response) => {
+    //   chatMessagesWrapper.scrollTop = chatMessagesWrapper.scrollHeight;
+      console.log(response);
+      chatMessagesWrapper.innerHTML = "";
+    response.data.messages.forEach((message) => {
+      let messageElement = document.createElement("div");
+      messageElement.className = "message";
+      
+      let firstSpaceIndex = message.indexOf(',');
+      if (firstSpaceIndex !== -1) {
+        let firstWord = message.substring(0, firstSpaceIndex);
+        let restOfMessage = message.substring(firstSpaceIndex + 1);
+        
+        messageElement.innerHTML = `<strong>${firstWord}</strong> ${restOfMessage}`;
+      } else {
+        messageElement.textContent = message;
+      }
+      
+      chatMessagesWrapper.appendChild(messageElement);
+    });
+    });
+}, 1000);
+
 async function initPartyChat() {
-	let ytSecondarySection = await waitForElement("#secondary-inner");
+  let ytSecondarySection = await waitForElement("#secondary-inner");
 
-	let ytChatContainer = await waitForElement("#chat-container");
+  let ytChatContainer = await waitForElement("#chat-container");
 
-	ytSecondarySection.insertBefore(partyChatContainer, ytChatContainer);
+  ytSecondarySection.insertBefore(partyChatContainer, ytChatContainer);
 }
 // sleep(5000).then(() => { document.querySelector("#secondary-inner").insertBefore(x,document.querySelector("#chat-container")); });
